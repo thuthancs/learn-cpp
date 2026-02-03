e782d5852c7bf0b1ed21ef09b3fb5d89eebf2e32
Thu Than <than@uni.minerva.edu>
2026-02-03 14:21:06 -0800

    feat: added data type exercise

---
+++ arithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    float b = 1.5;
+    double c = 5.25;
+    double sum;
+
+    sum = a + b + c;
+
+    cout << "Sum of a, b, and c is " << sum << endl;
+    return 0;
+}
